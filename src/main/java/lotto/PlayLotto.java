b19a55d4a3ddb4713b544f48923c6cb9ba7c1565
SangHoon Park <sh.park@dongwha.com>
2024-04-30 09:23:57 +0900

    refactor: 주석 추가

--- src/main/java/lotto/PlayLotto.java
+++ src/main/java/lotto/PlayLotto.java
@@ -1,20 +1,34 @@
 package lotto;
 
+import lotto.view.InputView;
+import lotto.view.ResultView;
+
 public class PlayLotto {
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        ResultView resultView = new ResultView();
+        try {
+            InputView inputView = new InputView();
+            ResultView resultView = new ResultView();
 
-        Integer money = inputView.insertMoney();
+            // 구매할 금액 입력
+            Integer money = inputView.insertMoney();
 
-        Lotto lotto = new Lotto(money);
+            Lotto lotto = new Lotto(money);
 
-        resultView.printPurchaseInfo(lotto);
+            // 구매한 개수 및 구매 번호 출력
+            resultView.printPurchaseInfo(lotto);
 
-        String winningNumbers = inputView.insertLastWeekWinningNumber();
+            // 지난 주 당첨 번호 입력
+            String winningNumbers = inputView.insertLastWeekWinningNumber();
 
-        lotto.getWinningCount(winningNumbers);
+            // 지난 주 당첨 번호 유효성 검사
+            lotto.checkValidation(winningNumbers);
 
-        resultView.printResult(lotto);
+            // 로또 계산
+            lotto.calculateLotto(winningNumbers);
+
+            resultView.printResult(lotto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
