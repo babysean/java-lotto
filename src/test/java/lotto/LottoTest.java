739130f3127e0e1d8ecb9d624919fc3746bc7497
SangHoon Park <sh.park@dongwha.com>
2024-04-30 09:22:53 +0900

    test: 테스트 케이스 추가
     - 구매_금액은_1000_과_같거나_커야_합니다
     - 구매_금액은_숫자만_입력할_수_있습니다
     - 당첨번호는_콤마로_구분된_6개_정수_입니다
     - 각_당첨번호는_1과_45_사이의_정수_입니다
     - 당첨번호에는_중복된_번호가_없어야_합니다
     - 중복이_없는_6개의_난수를_생성합니다
     - 전달받은_금액으로_살_수_있는_로또의_개수를_반환합니다
     - 두_정수_배열의_일치하는_정수의_개수를_반환합니다
     - 정수_6개로_이루어진_문자열인지_확인합니다
     - 정수_리스트에서_특정_숫자에_맞는_값의_개수를_반환합니다

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,10 +6,80 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoTest {
+
+    @Test
+    @DisplayName("구매_금액은_1000_과_같거나_커야_합니다")
+    void 구매_금액은_1000_과_같거나_커야_합니다() {
+        // given
+        Integer money = 1000;
+
+        // when & then
+        assertThat(money).isGreaterThanOrEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("구매_금액은_숫자만_입력할_수_있습니다")
+    void 구매_금액은_숫자만_입력할_수_있습니다() {
+        // given
+        String money = "1000";
+
+        // when & then
+        assertThat(money).matches("\\d+");
+    }
+
+    @Test
+    @DisplayName("당첨번호는_콤마로_구분된_6개_정수_입니다")
+    void 당첨번호는_콤마로_구분된_6개_정수_입니다() {
+        // given
+        String winningNumbers = "1,2,3 ,4,5,6";
+        winningNumbers = winningNumbers.replace(" ", "");
+
+        // when & then
+        assertThat(winningNumbers).matches("\\d+,\\d+,\\d+,\\d+,\\d+,\\d+");
+    }
+
+    @Test
+    @DisplayName("각_당첨번호는_1과_45_사이의_정수_입니다")
+    void 각_당첨번호는_1과_45_사이의_정수_입니다() {
+        // given
+        String winningNumbers = "1,2,3,4,5,45";
+        String[] winningNumber = winningNumbers
+                .replace(" ", "")
+                .split(",");
+
+        // when
+        boolean result = Arrays
+                .stream(winningNumber)
+                .mapToInt(Integer::parseInt)
+                .allMatch(number -> number >= 1 && number <= 45);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("당첨번호에는_중복된_번호가_없어야_합니다")
+    void 당첨번호에는_중복된_번호가_없어야_합니다() {
+        // given
+        String winningNumber = "1,2,3,4,5,45";
+        String[] winningNumbers = winningNumber
+                .replace(" ", "")
+                .split(",");
+
+        // when
+        long count = Arrays.stream(winningNumbers)
+                .mapToInt(Integer::parseInt)
+                .distinct()
+                .count();
+
+        // then
+        assertThat(count).isEqualTo(6);
+    }
 
     @Test
     @DisplayName("중복이_없는_6개의_난수를_생성합니다")
@@ -35,13 +105,13 @@ class LottoTest {
     void 전달받은_금액으로_살_수_있는_로또의_개수를_반환합니다() {
         // given
         final int LOTTO_PRICE = 1000;
-        int money = 500;
+        int money = 1500;
 
         // when
         int count = money / LOTTO_PRICE;
 
         // then
-        assertThat(count).isEqualTo(0);
+        assertThat(count).isEqualTo(1);
     }
 
     @Test
@@ -63,24 +133,24 @@ class LottoTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"1, 2, 3, 4, 5, 6, ", "1, 2, 3, 4, 5, 6", "1,2,3,4,5,6", "    1,2   ,3,4,5       ,6"})
-    @DisplayName("String으로_전달받은_숫자를_List으로_변환합니다")
-    void String으로_전달받은_숫자를_List으로_변환합니다(String numbers) {
+    @DisplayName("정수_6개로_이루어진_문자열인지_확인합니다")
+    void 정수_6개로_이루어진_문자열인지_확인합니다(String numbers) {
         // given
         numbers = numbers.replace(" ", "");
-        List<Integer> lottoNumbers = new ArrayList<>();
+        List<Integer> lottoNumbers;
 
         // when
-        for (String number : numbers.split(",")) {
-            lottoNumbers.add(Integer.parseInt(number));
-        }
+        lottoNumbers = Arrays.stream(numbers.replace(" ", "").split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
 
         // then
         assertThat(lottoNumbers.size()).isEqualTo(6);
     }
 
     @Test
-    @DisplayName("정수_LIST_에서_숫자에_맞는_값의_개수를_반환합니다")
-    void 정수_LIST_에서_숫자에_맞는_값의_개수를_반환합니다() {
+    @DisplayName("정수_리스트에서_특정_숫자에_맞는_값의_개수를_반환합니다")
+    void 정수_리스트에서_특정_숫자에_맞는_값의_개수를_반환합니다() {
         // given
         List<Integer> lottoNumbers = Arrays.asList(1, 2, 3, 4, 3, 6);
         int number = 3;
@@ -91,5 +161,4 @@ class LottoTest {
         // then
         assertThat(count).isEqualTo(2);
     }
-
 }
