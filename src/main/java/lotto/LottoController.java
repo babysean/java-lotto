ac8d76edb62ae4e688e3d471ac1dc973f835993f
SangHoon Park <tkdgns7070@gmail.com>
2024-05-06 14:24:59 +0900

    refactor: PlayLotto 클래스 이름 변경
     - PlayLotto -> LottoController

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -5,13 +5,14 @@ import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.ResultView;
 
-public class PlayLotto {
+public class LottoController {
     public static void main(String[] args) {
         ResultView resultView = new ResultView();
         InputView inputView = new InputView();
         PurchasedNumbers purchasedNumbers = new PurchasedNumbers();
 
         LottoService lottoService = new LottoService(inputView, resultView, purchasedNumbers);
+
         lottoService.run();
     }
 }
