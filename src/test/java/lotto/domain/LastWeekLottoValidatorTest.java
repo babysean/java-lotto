ffeae5104857162b92cc2439c5ff32deff48275c
SangHoon Park <sh.park@dongwha.com>
2024-05-10 16:31:12 +0900

    test: 테스트 코드 작성

---
+++ src/test/java/lotto/domain/LastWeekLottoValidatorTest.java
@@ -0,0 +1,58 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+@DisplayName("지난 주 당첨 로또 번호의 유효성을 테스트 합니다.")
+class LastWeekLottoValidatorTest {
+    @Test
+    @DisplayName("당첨번호는_콤마로_구분된_숫자_6개이어야_합니다")
+    void 당첨번호는_콤마로_구분된_숫자_6개이어야_합니다() {
+        // given
+        String winningNumber = "1,2,3,4,5,s";
+        String [] winningNumbers = winningNumber
+                .replace(" ", "")
+                .split(",");
+
+        // when
+        LastWeekLottoValidator lastWeekLottoValidator = new LastWeekLottoValidator();
+
+        // then
+        assertThatIllegalArgumentException().isThrownBy(() -> lastWeekLottoValidator.validate(winningNumbers));
+    }
+
+    @Test
+    @DisplayName("각_당첨번호는_1과_45_사이의_정수이어야_합니다")
+    void 각_당첨번호는_1과_45_사이의_정수이어야_합니다() {
+        // given
+        String winningNumber = "1,2,3,4,5,70";
+        String [] winningNumbers = winningNumber
+                .replace(" ", "")
+                .split(",");
+
+        // when
+        LastWeekLottoValidator lastWeekLottoValidator = new LastWeekLottoValidator();
+
+        // then
+        assertThatIllegalArgumentException().isThrownBy(() -> lastWeekLottoValidator.validate(winningNumbers));
+    }
+
+    @Test
+    @DisplayName("당첨번호에_중복된_숫자가_없어야_합니다")
+    void 당첨번호에_중복된_숫자가_없어야_합니다() {
+        // given
+        String winningNumber = "1,2,3,4,5,5";
+        String [] winningNumbers = winningNumber
+                .replace(" ", "")
+                .split(",");
+
+        // when
+        LastWeekLottoValidator lastWeekLottoValidator = new LastWeekLottoValidator();
+
+        // then
+        assertThatIllegalArgumentException().isThrownBy(() -> lastWeekLottoValidator.validate(winningNumbers));
+    }
+
+}
--- src/test/java/lotto/domain/LottoCalculatorTest.java
+++ src/test/java/lotto/domain/LottoCalculatorTest.java
@@ -1,16 +1,20 @@
 package lotto.domain;
 
+import lotto.service.LottoService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 class LottoCalculatorTest {
     @Test
-    @DisplayName("두_정수_리스트의_일치하는_정수의_개수를_반환합니다")
-    void 두_정수_리스트의_일치하는_정수의_개수를_반환합니다() {
-        // TODO : 테스트 코드 작성
-
-        /*// given
-        int purchaseMoney = 2000;
+    @DisplayName("전달_받은_숫자만큼_일치하는_티켓의_개수를_반환합니다")
+    void 전달_받은_숫자만큼_일치하는_티켓의_개수를_반환합니다() {
+        // given
         List<LottoTicket> lottoTickets = new ArrayList<>();
         lottoTickets.add(new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6)));
         lottoTickets.add(new LottoTicket(Arrays.asList(7, 8, 9, 10, 11, 12)));
@@ -18,41 +22,21 @@ class LottoCalculatorTest {
         LottoTicket winningLottoTicket = new LottoTicket(Arrays.asList(2, 3, 4, 5, 20, 21));
 
         // when
-        LottoCalculator calculator = new LottoCalculator(lottoTickets, winningLottoTicket);
-        List<Integer> matchingCount = calculator.calculate();
+        LottoCalculator calculator = new LottoCalculator();
+
+        List<Integer> matchingCount = calculator.calculate(lottoTickets, winningLottoTicket);
+
+        int count = calculator.getCountOfWin(matchingCount, 4);
 
         // then
-        assertThat(calculator.getCountOfWins(matchingCount,4)).isEqualTo(1);*/
-    }
-
-    @Test
-    @DisplayName("정수_리스트에서_특정_숫자에_맞는_값의_개수를_반환합니다")
-    void 정수_리스트에서_특정_숫자에_맞는_값의_개수를_반환합니다() {
-        // TODO : 테스트 코드 작성
-
-        /*// given
-        int purchaseMoney = 2000;
-        List<LottoTicket> lottoTickets = new ArrayList<>();
-        lottoTickets.add(new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6)));
-        lottoTickets.add(new LottoTicket(Arrays.asList(7, 8, 9, 10, 11, 12)));
-
-        LottoTicket winningLottoTicket = new LottoTicket(Arrays.asList(2, 3, 4, 5, 20, 21));
-
-        // when
-        LottoCalculator calculator = new LottoCalculator(lottoTickets, winningLottoTicket);
-
-        int count = calculator.getCountOfWins(4);
-
-        // then
-        assertThat(count).isEqualTo(1);*/
+        assertThat(count).isEqualTo(1);
     }
 
     @Test
     @DisplayName("수익률을_계산하여_반환합니다")
     void 수익률을_계산하여_반환합니다() {
-        // TODO : 테스트 코드 작성
-
-        /*// given
+        // given
+        int moeny = 2000;
         List<LottoTicket> lottoTickets = new ArrayList<>();
         lottoTickets.add(new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6)));
         lottoTickets.add(new LottoTicket(Arrays.asList(7, 8, 9, 10, 11, 12)));
@@ -60,9 +44,12 @@ class LottoCalculatorTest {
         LottoTicket winningLottoTicket = new LottoTicket(Arrays.asList(2, 3, 4, 5, 20, 21));
 
         // when
-        LottoCalculator calculator = new LottoCalculator(lottoTickets, winningLottoTicket);
+        LottoService lottoService = new LottoService();
+
+        LottoCalculator calculator = new LottoCalculator();
+        List<Integer> matchingCount = calculator.calculate(lottoTickets, winningLottoTicket);
 
         // then
-        assertThat(calculator.getRateOfReturn()).isEqualTo(2.5);*/
+        assertThat(lottoService.getProfit(matchingCount, moeny)).isEqualTo(25.0);
     }
 }
