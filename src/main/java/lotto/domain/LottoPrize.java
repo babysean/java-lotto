ceb51322dfbbc25ad91a750e237d52a3802548e5
SangHoon <tkdgns7070@gmail.com>
2024-05-23 08:15:17 +0900

    chore: enum 수정
    - 2등 (5개 맞고 보너스 맞고) 에 대한 값 추가
    - 정수형을 실수형으로 변경

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -1,20 +1,21 @@
 package lotto.domain;
 
 public enum LottoPrize {
-    THREE_MATCHES(3, 5_000),
-    FOUR_MATCHES(4, 50_000),
-    FIVE_MATCHES(5, 1_500_000),
-    SIX_MATCHES(6, 2_000_000_000);
+    THREE_MATCHES(3.0, 5_000),
+    FOUR_MATCHES(4.0, 50_000),
+    FIVE_MATCHES(5.0, 1_500_000),
+    FIVE_AND_BONUS_MATCHES(5.1, 30_000_000),
+    SIX_MATCHES(6.0, 2_000_000_000);
 
-    private final int matches;
+    private final double matches;
     private final int prize;
 
-    LottoPrize(int matches, int prize) {
+    LottoPrize(double matches, int prize) {
         this.matches = matches;
         this.prize = prize;
     }
 
-    public int getMatches() {
+    public double getMatches() {
         return matches;
     }
 
