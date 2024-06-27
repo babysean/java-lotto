02dde32e636b7a36496c97b47e500aa4432def15
SangHoon <tkdgns7070@gmail.com>
2024-06-27 10:47:54 +0900

    test: 테스트 코드 추가

--- src/test/java/lotto/domain/LottoConsumerTest.java
+++ src/test/java/lotto/domain/LottoConsumerTest.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -21,5 +23,38 @@ class LottoConsumerTest {
 
         // then
         assertThat(LottoTickets).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("수동_로또_티켓을_구매_합니다")
+    void 수동_로또_티켓을_구매_합니다() {
+        // given
+        LottoTicket lottoTicket_1 = new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6));
+        LottoTicket lottoTicket_2 = new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+        List<LottoTicket> LottoTickets = new ArrayList<>();
+        LottoTickets.add(lottoTicket_1);
+        LottoTickets.add(lottoTicket_2);
+
+        // when
+        LottoConsumer consumer =  new LottoConsumer();
+        consumer.buyManualLotto(LottoTickets);
+
+        // then
+        assertThat(consumer.getManualLottoTicketsCount()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("자동_로또_티켓_수를_반환_합니다")
+    void 자동_로또_티켓_수를_반환_합니다() {
+        // given
+        int money = 3000;
+
+        // when
+        LottoConsumer consumer =  new LottoConsumer();
+        consumer.buyLotto(money);
+
+        // then
+        assertThat(consumer.getAutoLottoTicketsCount()).isEqualTo(3);
     }
 }
