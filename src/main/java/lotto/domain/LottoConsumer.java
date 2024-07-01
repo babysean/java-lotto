f0e2d5cdfe5b99dde281ea77bc12f3b1e34ed75f
SangHoon <tkdgns7070@gmail.com>
2024-07-01 09:42:29 +0900

    chore: 로또 티켓 합치기
    - 수동, 자동 로또 티켓으로 나누지 않고 하나로 관리

--- src/main/java/lotto/domain/LottoConsumer.java
+++ src/main/java/lotto/domain/LottoConsumer.java
@@ -7,11 +7,11 @@ import java.util.List;
 
 public class LottoConsumer {
 
-    /** 자동 구매한 로또 티켓 목록 */
-    private final List<LottoTicket> autoLottoTickets = new ArrayList<>();
+    /** 구매한 로또 티켓 목록 */
+    private final List<LottoTicket> lottoTickets = new ArrayList<>();
 
-    /** 수동 구매한 로또 티켓 목록 */
-    private final List<LottoTicket> manualLottoTickets = new ArrayList<>();
+    /** 수동 로또 구매 개수 */
+    private int manualLottoCount = 0;
 
     /**
      * 자동 로또 티켓을 구매합니다.
@@ -20,11 +20,11 @@ public class LottoConsumer {
      */
     public void buyLotto(int money) {
         int purchasedCount = money / LOTTO_PRICE;
-        int autoPurchasedCount = purchasedCount - manualLottoTickets.size();
+        int autoPurchasedCount = purchasedCount - manualLottoCount;
 
         for (int i = 0 ; i < autoPurchasedCount ; i++) {
             LottoGenerator generator = new LottoGenerator();
-            autoLottoTickets.add(new LottoTicket(generator.generate()));
+            lottoTickets.add(new LottoTicket(generator.generate()));
         }
     }
 
@@ -34,25 +34,8 @@ public class LottoConsumer {
      * @param lottoTickets 수동 로또 티켓 목록
      */
     public void buyManualLotto(List<LottoTicket> lottoTickets) {
-        manualLottoTickets.addAll(lottoTickets);
-    }
-
-    /**
-     * 자동 로또 티켓 수 반환 합니다.
-     *
-     * @return int
-     */
-    public int getAutoLottoTicketsCount() {
-        return autoLottoTickets.size();
-    }
-
-    /**
-     * 수동 로또 티켓 수 반환 합니다.
-     *
-     * @return int
-     */
-    public int getManualLottoTicketsCount() {
-        return manualLottoTickets.size();
+        this.lottoTickets.addAll(lottoTickets);
+        manualLottoCount = lottoTickets.size();
     }
 
     /**
@@ -61,9 +44,24 @@ public class LottoConsumer {
      * @return List<LottoTicket>
      */
     public List<LottoTicket> getLottoTickets() {
-        List<LottoTicket> allLottoTickets = new ArrayList<>(manualLottoTickets);
-        allLottoTickets.addAll(autoLottoTickets);
+        return lottoTickets;
+    }
 
-        return allLottoTickets;
+    /**
+     * 자동 로또 티켓 개수를 반환합니다.
+     *
+     * @return int
+     */
+    public int getAutoLottoTicketCount() {
+        return lottoTickets.size() - manualLottoCount;
+    }
+
+    /**
+     * 수동 로또 티켓 개수를 반환합니다.
+     *
+     * @return int
+     */
+    public int getManualLottoTicketCount() {
+        return manualLottoCount;
     }
 }
